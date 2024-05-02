0739dc2ef252986a90910836c3817f36aa243520
sorktjrrb <167832045+sorktjrrb@users.noreply.github.com>
2024-05-02 10:00:56 +0900

    v 1.0

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def main() :
             st.error('데이터의 갯수는 10개 이상이어야 합니다.')
             return
 
-        # 1-2. 유저한테 데이터프레임 보여준다.
+        # 1-3. 유저한테 데이터프레임 보여준다.
         st.dataframe(df)
 
         # 2. nan 데이터 있으면, 삭제하자.
@@ -96,17 +96,45 @@ def main() :
             # 이런식으로 로직을 풀어나가다가 수정사항이 있으면 유기적으로 바꾸면 된다.
             # if X.shape[0] < 10 : => 상단으로 올리기때문에 위에서는 X를 df로 바꾼다. 상단에선 X가 없기때문에..
             #     st.error('데이터의 갯수는 10개 이상이어야 합니다.')
+            wcss = []
+            for k in np.arange(1, 10 + 1) :
+                kmeans = KMeans(n_clusters = k, random_state=5)
+                kmeans.fit(X_new)
+                wcss.append(kmeans.inertia_)
 
 
             # 6. elbow method 를 이용해서, 차트로 보여준다.
+            fig1 = plt.figure()
+            X = np.arange(1, 10+1)
+            plt.plot(X, wcss)
+            plt.title('The Elbow Mehtod')
+            plt.xlabel('Number of Clusters')
+            plt.ylabel('WCSS')
+            st.pyplot(fig1)
 
             # 7. 유저가 k의 갯수를 정한다.
+            k = st.slider('클러스터 갯수 설정', 1, 10)
 
             # 8. KMeans 수행해서 그룹정보를 가져온다.
+            kmeans = KMeans(n_clusters= k, random_state= 5)
+            y_pred = kmeans.fit_predict(X_new)
 
             # 9. 원래 있던 df 에 Group 이라는 컬럼을 만들어준다.
+            df['Group'] = y_pred
 
             # 10. 결과를 파일로 저장한다.
+            df.to_csv('result_csv')
+
+            # 11. 유저에게 보여준다.
+            st.subheader('클러스터링 결과')
+
+            st.dataframe(df)
+
+            # 12. 유저가 그룹을 선택하면, 해당 그룹의 정보를 보여준다.
+            choice = st.selectbox('그룹을 선택하세요', np.arange(0, k))
+            
+            st.dataframe(df.loc[ df['Group'] == choice , ])
+
 
 if __name__ == '__main__' :
     main()
